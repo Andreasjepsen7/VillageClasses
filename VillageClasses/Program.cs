735cb6995b8dbeef44e71b4aaf50fd2727cae67c
Andreas <Andreas@LAPTOP-SPJCEGNK>
2023-10-30 10:48:10 +0100

    Mumble

--- VillageClasses/Program.cs
+++ VillageClasses/Program.cs
@@ -2,12 +2,12 @@
 {
     public string Name { get; set; }
     public int Age { get; set; }
-    public List<Inventory> Inventory { get; set; }
+    public List<InventoryItem> Inventory { get; set; }
     public int Money { get; set; }
     public string CurrentLocation { get; set; }
     public int Health { get; set; }
 
-    public Villager(string name, int age, List<Inventory> inventory, int money, string currentLocation, int health)
+    public Villager(string name, int age, List<InventoryItem> inventory, int money, string currentLocation, int health)
     {
         Name = name;
         Age = age;
@@ -25,10 +25,9 @@ public class Pillager : Villager
     public List<string> CombatSkills { get; set; }
     public List<string> Loot { get; set; }
 
-    public Pillager(string name, int age, List<Inventory> inventory, int money, string currentLocation, int health)
+    public Pillager(string name, int age, List<InventoryItem> inventory, int money, string currentLocation, int health)
         : base(name, age, inventory, money, currentLocation, health)
     {
-        // Initialize Pillager-specific attributes here
     }
 }
 
@@ -38,10 +37,9 @@ public class Paladins : Villager
     public string Armor { get; set; }
     public bool HasShield { get; set; }
 
-    public Paladins(string name, int age, List<Inventory> inventory, int money, string currentLocation, int health)
+    public Paladins(string name, int age, List<InventoryItem> inventory, int money, string currentLocation, int health)
         : base(name, age, inventory, money, currentLocation, health)
     {
-        // Initialize Paladins-specific attributes here
     }
 }
 
@@ -50,10 +48,9 @@ public class Peasant : Villager
     public string Job { get; set; }
     public List<TradeGood> TradeGoods { get; set; }
 
-    public Peasant(string name, int age, List<Inventory> inventory, int money, string currentLocation, int health)
+    public Peasant(string name, int age, List<InventoryItem> inventory, int money, string currentLocation, int health)
         : base(name, age, inventory, money, currentLocation, health)
     {
-        // Initialize Peasant-specific attributes here
     }
 }
 
@@ -72,7 +69,7 @@ public class King : Villager
     public string Title { get; set; }
     public string Enemy { get; set; }
 
-    public King(string name, int age, List<Inventory> inventory, int money, string currentLocation, int health, string title, string enemy)
+    public King(string name, int age, List<InventoryItem> inventory, int money, string currentLocation, int health, string title, string enemy)
         : base(name, age, inventory, money, currentLocation, health)
     {
         Title = title;
@@ -80,11 +77,45 @@ public class King : Villager
     }
 }
 
-public class Inventory
+public class InventoryItem
 {
-    public string Weapon { get; set; }
-    public string Food { get; set; }
-    public string Pony { get; set; }
+    public Weapon Weapon { get; set; }
+    public Food Food { get; set; }
+    public Pony Pony { get; set; }
+}
+
+public class Price
+{
+    public int Value { get; }
+
+    public Price(int value)
+    {
+        Value = value;
+    }
+}
+
+public class Weapon
+{
+    public string Name { get; set; }
+    public int Damage { get; set; }
+    public string Type { get; set; }
+    public Price price { get; set; }
+}
+
+public class Food
+{
+    public string Name { get; set; }
+    public int Nutrition { get; set; }
+    public Price price { get; set; }
+}
+
+public class Pony
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Color { get; set; }
+
+    public Price price { get; set; }
 }
 public class RNG
 {
